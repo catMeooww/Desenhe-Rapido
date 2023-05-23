8698ba8a701c38fffc55b343597251144710402b
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-05-23 15:50:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 array = ["plane","pencil","book","bottle","map","ladder"];
 random = Math.floor(Math.random()*array.length);
-draw = array[random]
-console.log(draw);
-document.getElementById("desenhe").innerHTML = draw;
+desenhe = array[random]
+console.log(desenhe);
+document.getElementById("desenhe").innerHTML = "Desenhe: " + desenhe;
 timerCounter = 0;
 timerCheck = "";
 drawSketch = "";
 answerHolder = "";
 score = 0;
+
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function clearCanvas(){
+    background("white");
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
