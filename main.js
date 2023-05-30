a5009534351e8558966d47e7d37f1fca5f51e9e0
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-05-30 15:52:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-array = ["plane","pencil","book","bottle","map","ladder"];
+array = ["plane","pencil","book","bottle","map","ladder","Star","Clock"];
 random = Math.floor(Math.random()*array.length);
 desenhe = array[random]
 console.log(desenhe);
@@ -8,11 +8,15 @@ timerCheck = "";
 drawSketch = "";
 answerHolder = "";
 score = 0;
-
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
 function setup(){
     canvas = createCanvas(280,280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
 function clearCanvas(){
     background("white");
@@ -23,4 +27,19 @@ function draw(){
     if (mouseIsPressed) {
         line(pmouseX,pmouseY,mouseX,mouseY);
     }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResults);
+}
+function gotResults(error,results){
+    if (error) {
+        console.error(error);
+    }
+    console.log(results)
+    result = results[0].label;
+    confidence = Math.round(results[0].confidence * 100);
+    document.getElementById("label").innerHTML = result;
+    document.getElementById("confidence").innerHTML = confidence;
+    UtterThis = new SpeechSynthesisUtterance(result.replace("_"," "));
+    synth.speak(UtterThis);
 }
